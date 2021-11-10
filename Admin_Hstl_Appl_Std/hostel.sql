9e89a427080fbd6893ded788baf6a780236c25f7
Anushka Pathak <75501450+anumshka@users.noreply.github.com>
2021-11-10 18:05:21 +0530

    Update hostel.sql

--- Admin_Hstl_Appl_Std/hostel.sql
+++ Admin_Hstl_Appl_Std/hostel.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Hostel (
+CREATE TABLE hostel (
   hostel_id int PRIMARY KEY,
   hostel_name varchar(255),
   phone_no int, 	
@@ -10,8 +10,8 @@ CREATE TABLE Hostel (
   on delete set null
 );
 
-INSERT INTO Hostel(hostel_id, hostel_name, phone_no, no_of_rooms, no_of_students,admin_id) VALUES 
+INSERT INTO hostel(hostel_id, hostel_name, phone_no, no_of_rooms, no_of_students,admin_id) VALUES 
 ( '1','B',0731224455,200,NULL,1),
 ( '2','G',0731225544,200,NULL,2);
 
-select * from Hostel;
+select * from hostel;
