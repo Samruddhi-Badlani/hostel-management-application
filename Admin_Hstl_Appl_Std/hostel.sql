ab337bfd4ad67c528e99491d01c4f9ed2bf607a5
anumshka <anushka.mpathak17@gmail.com>
2021-11-10 17:40:04 +0530

    last one

--- Admin_Hstl_Appl_Std/hostel.sql
+++ Admin_Hstl_Appl_Std/hostel.sql
@@ -2,8 +2,8 @@ CREATE TABLE Hostel (
   hostel_id int PRIMARY KEY,
   hostel_name varchar(255),
   phone_no int, 	
-  no_of_rooms varchar(255),
-  no_of_students varchar(255),
+  no_of_rooms int,
+  no_of_students int,
   admin_id int 
   references administrator(admin_id)
   on update cascade
@@ -11,7 +11,7 @@ CREATE TABLE Hostel (
 );
 
 INSERT INTO Hostel(hostel_id, hostel_name, phone_no, no_of_rooms, no_of_students,admin_id) VALUES 
-( '1','B',0731224455,'200',NULL,1),
-( '2','G',0731225544,'200',NULL,2);
+( '1','B',0731224455,200,NULL,1),
+( '2','G',0731225544,200,NULL,2);
 
 select * from Hostel;
