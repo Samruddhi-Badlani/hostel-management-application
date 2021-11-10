fa834b27b9196889f32760f1a41e738d750ecab3
Anushka Pathak <75501450+anumshka@users.noreply.github.com>
2021-11-10 18:06:47 +0530

    Update student.sql

--- Admin_Hstl_Appl_Std/student.sql
+++ Admin_Hstl_Appl_Std/student.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Student (
+CREATE TABLE student (
   student_id serial PRIMARY KEY,
   Fname varchar(255),
   Lname varchar(255),
@@ -26,7 +26,7 @@ CREATE TABLE Student (
   on delete set null
 );	
 
-INSERT INTO Student (Fname, Lname, phone_no, gender, year_of_study, date_of_birth, aadhaar_no,home_address,room_id,hostel_id,dept_id,certificate_id )VALUES
+INSERT INTO student (Fname, Lname, phone_no, gender, year_of_study, date_of_birth, aadhaar_no,home_address,room_id,hostel_id,dept_id,certificate_id )VALUES
 ('Prajwal','Ghoradkar','9898989898','M','2019-2023','2000-02-01','1231 2121 2000',NULL,1,1,1,860753),
 ('Shivansh','Tiwari','9444989898','M','2019-2023','2001-01-01','1111 2001 5200',NULL,2,2,2,127381),
 ('Alia','Lambore','9856489652','F','2020-2024','2002-11-09','5231 6875 2000',NULL,3,1,3,218982),
@@ -34,4 +34,4 @@ INSERT INTO Student (Fname, Lname, phone_no, gender, year_of_study, date_of_birt
 ('Chetali','Vishwas','8596456321','F','2019-2023','2001-05-04','3256 2121 6598',NULL,5,1,5,574839),
 ('Gurpreet','Kaur','9545625463','F','2019-2023','2000-02-06','2000 5642 4651',NULL,6,2,6,639575);
 
-select * from Student;
+select * from student;
